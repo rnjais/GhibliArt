bad12183e488c558c6cb0c22de9f0e454f4849e0
Aryan Jaiswal <your-github-aj80529754@gmail.com>
2026-07-21 20:52:17 +0530

    Initial Spring Boot project

---
+++ ghibliapi/src/main/java/in/aryan/ghibliapi/GhibliapiApplication.java
@@ -0,0 +1,13 @@
+package in.aryan.ghibliapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhibliapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhibliapiApplication.class, args);
+	}
+
+}
