54c82d6c1729e27758c46839424bcde234af15bf
Aryan Jaiswal <your-github-aj80529754@gmail.com>
2026-07-21 21:27:30 +0530

    Commit

--- ghibliapi/src/main/java/in/aryan/ghibliapi/GhibliapiApplication.java
+++ ghibliapi/src/main/java/in/aryan/ghibliapi/GhibliapiApplication.java
@@ -2,8 +2,10 @@ package in.aryan.ghibliapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class GhibliapiApplication {
 
 	public static void main(String[] args) {
