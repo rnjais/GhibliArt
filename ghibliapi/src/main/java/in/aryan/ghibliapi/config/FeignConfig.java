ea27f49f4123e5d362eccc733284a41e7dde11dc
Aryan Jaiswal <your-github-aj80529754@gmail.com>
2026-07-22 21:39:31 +0530

    FeignConfig.java

--- ghibliapi/src/main/java/in/aryan/ghibliapi/config/FeignConfig.java
+++ ghibliapi/src/main/java/in/aryan/ghibliapi/config/FeignConfig.java
@@ -2,6 +2,7 @@ package in.aryan.ghibliapi.config;
 
 import feign.form.spring.SpringFormEncoder;
 import org.springframework.cloud.openfeign.support.SpringEncoder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverters;
 import org.springframework.web.client.RestTemplate;
@@ -11,6 +12,7 @@ import java.beans.Encoder;
 @Configuration
 public class FeignConfig {
 
+    @Bean
     public Encoder feignformEncoder(){
         return new SpringFormEncoder(
                 new SpringEncoder(()-> new HttpMessageConverters(new RestTemplate().getMessageConverters())
